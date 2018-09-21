e5ae79481e49c6faac08404e7d5f47ad4daebfe5
Rose Sorfa <rose.otter.2015@gmail.com>
2018-09-21 15:40:44 -0500

    fix css, update readme

--- src/components/setup-page.js
+++ src/components/setup-page.js
@@ -44,22 +44,18 @@ export class SetupPage extends React.Component {
             <form 
               className="setup-form"
               onSubmit={e => this.onSubmit(e)} 
-              ref={input => (this.input = input)}>
-            <div className="setup">   
-                <div className="daily-goal">
-                <label htmlFor="daily-goal">Daily goal: </label>
-                <input name="daily-goal" type="text" id="goal-input" value={this.state.dailyGoal}
-                    onChange={e => this.setGoals(e.target.value)}/> 
-                </div>
-                <div className="total-days">
-                <label htmlFor="total-days">Days until deadline: </label>
-                    <input name="total-days" type="text" id="deadline-input" value={this.state.totalDays} 
-                    onChange={e => this.setDays(e.target.value)}/>
-                </div>
-            </div>
-            <div className="button">
+              ref={input => (this.input = input)}> 
+                    <div className="daily-goal">
+                    <label htmlFor="daily-goal">Daily goal: </label>
+                    <input name="daily-goal" type="text" id="goal-input" value={this.state.dailyGoal}
+                        onChange={e => this.setGoals(e.target.value)}/> 
+                    </div>
+                    <div className="total-days">
+                    <label htmlFor="total-days">Days until deadline: </label>
+                        <input name="total-days" type="text" id="deadline-input" value={this.state.totalDays} 
+                        onChange={e => this.setDays(e.target.value)}/>
+                    </div>
                 <button type="submit">Submit</button> 
-            </div>
             </form> 
           </main>
         );
